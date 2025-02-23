2bd063e68338475f61d4d2ee6507afed1a3fec75
Anthony Pham <gm.pham@gmail.com>
2025-02-23 08:45:39 -0500

    verify setup and teardown scripts work

--- teardown/dev-teardown.ps1
+++ teardown/dev-teardown.ps1
@@ -4,9 +4,10 @@
 kubectl delete service xp-flask-service
 kubectl delete deployment xp-flask-deployment
 kubectl delete secrets dev-secrets
-kubectl delete pvc data-xp-postgres-postgresql-0
 
 # Need to scale down or resource will replicate and prevent helm uninstall
 kubectl scale statefulset xp-postgres-postgresql --replicas=0
+
+kubectl delete pvc data-xp-postgres-postgresql-0
 
 helm uninstall xp-postgres
